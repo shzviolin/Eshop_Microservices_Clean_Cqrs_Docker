c6ba1f3613babb9f1e5f751e100c13d511e16177
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-06-15 20:16:24 +0330

    01_Create project and folders, install Docker and run Redis and RabbitMq on Docker

---
+++ Src/Services/IDPService/IDP.Api/Controllers/V1/UserController.cs
@@ -0,0 +1,12 @@
+﻿using IDP.Api.Controllers.BaseController;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Api.Controllers.V1
+{
+    [Route("api/v1/user")]
+    [ApiController]
+    public class UserController : IBaseController
+    {
+    }
+}
